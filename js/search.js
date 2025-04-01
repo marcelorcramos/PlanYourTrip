dce10f66f22232a5dae7eb749438f903be205a30
Marcelo Ramos <marcelorcramos@gmail.com>
2025-04-01 09:43:04 +0100

    Update search.js

--- js/search.js
+++ js/search.js
@@ -152,8 +152,8 @@ const destinations = [
     {
         id: '16',
         location: 'Bangladesh',
-        imagem: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fmundoeducacao.uol.com.br%2Fgeografia%2Fafeganistao.htm&psig=AOvVaw0ZbGRcUO1Ro4mmDF2M1QdC&ust=1741989848988000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCKDNsYqIiIwDFQAAAAAdAAAAABAE',
-        imagemmapa:'',
+        imagem: 'https://moverdb.com/wp-content/uploads/dhaka-bangladesh.jpg',
+        imagemmapa:'https://www.worldatlas.com/r/w1200/upload/46/66/7c/bd-01.jpg',
         price: 120,
         provider: 'Booking.com',
         link: 'https://www.booking.com',
