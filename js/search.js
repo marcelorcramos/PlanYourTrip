cbf41d7ef4dd17f7c750060bf5f969150a807b8a
Marcelo Ramos <marcelorcramos@gmail.com>
2025-04-05 08:42:57 +0100

    Update search.js

--- js/search.js
+++ js/search.js
@@ -231,8 +231,8 @@ const destinations = [
     {
         id: '24',
         location: 'Botsuana',
-        imagem: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fmundoeducacao.uol.com.br%2Fgeografia%2Fafeganistao.htm&psig=AOvVaw0ZbGRcUO1Ro4mmDF2M1QdC&ust=1741989848988000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCKDNsYqIiIwDFQAAAAAdAAAAABAE',
-        imagemmapa:'',
+        imagem: 'https://www.doisniveis.com/wp-content/uploads/2021/10/justice-hubane-OjmO-dNF0lQ-unsplash-1024x683.jpg',
+        imagemmapa:'https://cdn.britannica.com/19/4219-050-BE3F288D/Botswana-map-boundaries-cities-locator.jpg',
         price: 120,
         provider: 'Booking.com',
         link: 'https://www.booking.com',
@@ -241,8 +241,8 @@ const destinations = [
     {
         id: '25',
         location: 'Brasil',
-        imagem: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fmundoeducacao.uol.com.br%2Fgeografia%2Fafeganistao.htm&psig=AOvVaw0ZbGRcUO1Ro4mmDF2M1QdC&ust=1741989848988000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCKDNsYqIiIwDFQAAAAAdAAAAABAE',
-        imagemmapa:'',
+        imagem: 'https://media-front.elmostrador.cl/2022/10/BRASIL-03-Rio-de-Janeiro-700x467.jpg',
+        imagemmapa:'https://www.worldometers.info/img/maps/brazil_physical_map.gif',
         price: 120,
         provider: 'Booking.com',
         link: 'https://www.booking.com',
